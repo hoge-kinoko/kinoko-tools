939dd02ed18bf1b72b3d98f2d63f95c227a0572a
nyahamup <166373079+nyahamup@users.noreply.github.com>
2024-09-12 22:07:31 +0900

    :+1: ガチャレベルの判定、コース設定を追加

--- rush_calc/main.rb
+++ rush_calc/main.rb
@@ -1,16 +1,18 @@
 require "js"
 
 class EventCalc
-  # TODO: これ使うようにしたい
   # コストが大きいものから計算
-  ticket_courses = [
-    { cost: 800, draw_count: 999 },
-    { cost: 30, draw_count: 35 },
+  TICKET_COURSES = [
+    { cost: 800, course: 999, level: 15 },
+    { cost: 30, course: 35, level: 1 },
+    { cost: 15, course: 15, level: 1 },
   ].freeze
 
-  stone_courses = [
-    { cost: 600, draw_count: 35 },
-  ]
+  STONE_COURSES = [
+    { cost: 16_000, course: 999, level: 15},
+    { cost: 600, course: 35, level: 1 },
+    { cost: 300, course: 15, level: 1 },
+  ].freeze
 
   def initialize
     add_calc_event
@@ -30,15 +32,87 @@ class EventCalc
     document.getElementById("stone_num")[:value].to_i
   end
 
-  def calc_by_ticket
-    course_999 = (ticket_num / 800).floor * 999
-    course_35 = ((ticket_num % 800) / 30).floor * 35
+  def level
+    document.getElementById("level")[:checked].to_s == "true" ? 15 : 1
+  end
 
-    course_999 + course_35
+  def delimited(n)
+    n.to_s.reverse.scan(/.{1,3}/).join(",").reverse
+  end
+
+
+  def create_elem(tag, parent)
+    elem = document.createElement(tag)
+
+    yield(elem) if block_given?
+
+    parent.appendChild(elem) if parent
+
+    elem
+  end
+
+  # result
+  # { draw_count_by_course: { 999: 1, 35: 1 }, total_count: 1034, total_cost: 830}
+  def calc(courses, point)
+    result = {
+      total_draw_count: 0,
+      total_cost: 0,
+      draw_count_by_course: {},
+    }
+
+    courses.each do |course|
+      # レベルが足りていなければスキップ
+      next if level < course[:level]
+
+      draw_count = ((point - result[:total_cost]) / course[:cost]).floor
+
+      result[:draw_count_by_course][course[:course]] = draw_count
+      result[:total_draw_count] += draw_count * course[:course]
+      result[:total_cost] += draw_count * course[:cost]
+    end
+    result
   end
 
   def calc_by_stone
     (stone_num / 600).floor * 35
+  end
+
+  def create_result_table(parent)
+    ticket_result = calc(TICKET_COURSES, ticket_num)
+    stone_result = calc(STONE_COURSES, stone_num)
+
+    table = create_elem("table", parent)
+    tbody = create_elem("tbody", table)
+
+    {
+      "チケット" => ticket_result,
+      "ダイヤ" => stone_result,
+    }.each_pair do |item_name, result|
+      tr = create_elem("tr", tbody)
+
+      create_elem("td", tr) do |td|
+        td[:innerText] = item_name
+      end
+
+      create_elem("td", tr) do |td|
+        td[:innerText] = result[:draw_count_by_course].map do |(course, times)|
+          "#{course}: #{delimited(times)}回（合計: #{delimited(course * times)}）"
+        end.join("\n")
+      end
+
+      create_elem("td", tr) do |td|
+        td[:innerText] = "#{delimited(result[:total_draw_count])}回"
+      end
+    end
+
+    total_tr = create_elem("tr", tbody)
+    create_elem("td", total_tr) do |td|
+      td[:innerText] = "合計"
+    end
+    create_elem("td", total_tr) do |td|
+      td.setAttribute("colspan", "2")
+      td[:innerText] = "#{delimited(ticket_result[:total_draw_count] + stone_result[:total_draw_count])}回"
+    end
   end
 
   def add_calc_event
@@ -53,26 +127,7 @@ class EventCalc
         result.appendChild(span)
       end
 
-      document.createElement("table").tap do |table|
-        document.createElement("tbody").tap do |tbody|
-          [["チケット", calc_by_ticket], ["ダイヤ", calc_by_stone], ["合計", calc_by_ticket + calc_by_stone]].each do |content|
-            document.createElement("tr").tap do |tr|
-              document.createElement("td").tap do |td|
-                td[:innerText] = content[0]
-                tr.appendChild(td)
-              end
-              document.createElement("td").tap do |td|
-                td[:innerText] = "#{content[1].to_s.reverse.scan(/.{1,3}/).join(",").reverse}回"
-                tr.appendChild(td)
-              end
-              tbody.appendChild(tr)
-            end
-          end
-          table.appendChild(tbody)
-        end
-
-        result.appendChild(table)
-      end
+      create_result_table(result)
     end
   end
 end
